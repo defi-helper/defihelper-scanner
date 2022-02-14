768b8579f0fefd2a41274d61d96966b29525dc09
artur <amamedbekov@tokenomica.com>
2022-02-14 16:59:18 +0300

    fix

--- server/src/queueBroker.ts
+++ server/src/queueBroker.ts
@@ -1,29 +1,29 @@
-import 'module-alias/register';
-import cli from 'command-line-args';
-import container from './container';
+import "module-alias/register";
+import cli from "command-line-args";
+import container from "./container";
+
+async function handle() {
+  return container.model.queueService().handle();
+}
+
+function wait(time: number) {
+  return new Promise((resolve) => setTimeout(resolve, time));
+}
+
+const broker = (interval: number): any =>
+  handle().then((r) => wait(r ? 0 : interval).then(() => broker(interval)));
 
 container.model
   .migrationService()
   .up()
   .then(async () => {
     const options = cli([
-      { name: 'include', type: String, multiple: true, defaultValue: [] },
-      { name: 'exclude', type: String, multiple: true, defaultValue: [] },
-      { name: 'interval', type: Number, defaultValue: 1000 },
+      { name: "interval", type: Number, defaultValue: 1000 },
     ]);
     if (Number.isNaN(options.interval)) throw new Error(`Invalid interval`);
 
-    container.model
-      .queueService()
-      .createBroker({
-        interval: options.interval,
-        handler: {
-          include: options.include,
-          exclude: options.exclude,
-        },
-      })
-      .start();
-    container.logger().info(`Handle tasks with interval ${options.interval} ms`);
+    broker(options.interval);
+    container.logger().info(`Handle queue tasks`);
   })
   .catch((e) => {
     container.logger().error(e);
