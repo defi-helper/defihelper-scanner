0661d2aaa1c201df57d9b901ce750e7b3430b23a
artur <amamedbekov@tokenomica.com>
2022-05-27 19:18:22 +0300

    queue prefix

--- server/src/config/index.ts
+++ server/src/config/index.ts
@@ -19,7 +19,7 @@ export default {
       scheduledPublish: Boolean(process.env.RABBITMQ_SCHEDULED_PUBLISH ?? ''),
     },
     queues: [
-      { name: 'tasks_default', topic: 'tasks.*.default' },
+      { name: 'scanner_tasks_default', topic: 'tasks.*.default' },
     ],
   },
   blockchain: {
